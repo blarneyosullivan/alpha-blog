1eb6f00e9b1b8ab8586d37f1a9f771008213d358
blarney <blarneyosullivanbloggs@gmail.com>
2016-02-16 19:30:56 +0000

    add edit category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,6 +10,8 @@ class CategoriesController < ApplicationController
     @category = Category.new
   end
   
+  
+  
   def create
     @category = Category.new(category_params)
     
@@ -23,10 +25,32 @@ class CategoriesController < ApplicationController
   end
   
   
+  def edit
+    @category = Category.find(params[:id])  
+  end
+  
+  
+  def update
+    @category = Category.find(params[:id])
+    
+    if @category.update(category_params)
+      flash[:success] = "name was updated"
+      
+      redirect_to category_path(@category)
+    else
+      
+      render 'new'
+      
+    end
+  end
+  
   def show
     @category = Category.find(params[:id])
     @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
+  
+  
+  
   
   
   private
